2c4ef886b9b8ad389d90ca55d833511cb1996bb7
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-27 12:47:09 +0300

    add annotation package and create new controller

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    #[Route('/', name: 'app_question')]
+    public function homepage(): Response
+    {
+        return new Response('What a bewiching controller we have conjured!');
+    }
+}
