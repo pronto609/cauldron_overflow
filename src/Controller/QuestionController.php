cfaabb34696815dd433aa943fd14ceb7046bb442
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-28 08:24:31 +0300

    add aditional debug tool

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -17,7 +17,7 @@ class QuestionController extends AbstractController
     #[Route('/questions/{slug}')]
     public function show(string $slug = null)
     {
-
+        dump($this);
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'answers' => range(10, 20)
