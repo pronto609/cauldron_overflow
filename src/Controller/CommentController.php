55e5d5727aa6f08da8d27d3dd886adc366644e4e
Jurij Leshchuk <ddevdev609@gmail.com>
2023-09-28 10:27:45 +0300

    servisec

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Psr\Log\LoggerInterface;
 class CommentController extends AbstractController
 {
-    #[Route('/comments/{id}/vote/{direction}', name: 'app_comment')]
-    public function commentVote(int $id, string $direction): JsonResponse
+    #[Route('/comments/{id}/vote/{direction<up|down>}', name: 'app_comment', methods: ['POST'])]
+    public function commentVote($id, string $direction, LoggerInterface $logger): JsonResponse
     {
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
